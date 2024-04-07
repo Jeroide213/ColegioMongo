a5b3496c0a114ba81deef26da31fda73b84bfcd5
Jeroide213 <153406693+Jeroide213@users.noreply.github.com>
2024-04-07 15:09:44 -0300

    First Commit

--- src/main/java/com/example/ColegioMongo/Service/CursoService.java
+++ src/main/java/com/example/ColegioMongo/Service/CursoService.java
@@ -1,17 +1,22 @@
 package com.example.ColegioMongo.Service;
 
 import com.example.ColegioMongo.Models.Curso;
+import com.example.ColegioMongo.Models.Materia;
 import com.example.ColegioMongo.Repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 public class CursoService {
     @Autowired
     private CursoRepository cursoRepository;
+
+    @Autowired
+    private MateriaService materiaService;
 
     public List<Curso> obtenerTodos() {
         return cursoRepository.findAll();
@@ -31,6 +36,7 @@ public class CursoService {
     public void eliminar(Long id) {
         cursoRepository.deleteById(id);
     }
+
     public Optional<Curso> actualizar(Long id, Curso nuevoCurso) {
         Optional<Curso> cursoOptional = cursoRepository.findById(id);
         if (cursoOptional.isPresent()) {
@@ -38,6 +44,25 @@ public class CursoService {
             return Optional.of(cursoRepository.save(nuevoCurso));
         } else {
             return Optional.empty();
+        }
+    }
+    public Materia agregarMateriaACurso(Long cursoId, Long materiaId) {
+        Optional<Curso> cursoOptional = cursoRepository.findById(cursoId);
+        if (cursoOptional.isPresent()) {
+            Optional<Materia> materiaOptional = materiaService.obtenerPorId(materiaId);
+            if (materiaOptional.isPresent()) {
+                Curso curso = cursoOptional.get();
+                Materia materia = materiaOptional.get();
+                if (!curso.getMaterias().contains(materia)) {
+                    curso.getMaterias().add(materia);
+                    cursoRepository.save(curso);
+                }
+                return materia;
+            } else {
+                throw new NoSuchElementException("La materia con ID " + materiaId + " no existe");
+            }
+        } else {
+            throw new NoSuchElementException("El curso con ID " + cursoId + " no existe");
         }
     }
 }
