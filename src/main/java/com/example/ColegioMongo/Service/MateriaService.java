867199c7c3a75bc5d9d613f40644c39f70deccb6
Jeroide213 <153406693+Jeroide213@users.noreply.github.com>
2024-04-07 15:17:30 -0300

    First Commit

--- src/main/java/com/example/ColegioMongo/Service/MateriaService.java
+++ src/main/java/com/example/ColegioMongo/Service/MateriaService.java
@@ -1,10 +1,13 @@
 package com.example.ColegioMongo.Service;
 
 import com.example.ColegioMongo.Models.Materia;
+import com.example.ColegioMongo.Models.Profesor;
 import com.example.ColegioMongo.Repository.MateriaRepository;
+import com.example.ColegioMongo.Repository.ProfesorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +15,8 @@ import java.util.Optional;
 public class MateriaService {
     @Autowired
     private MateriaRepository materiaRepository;
+    @Autowired
+    private ProfesorRepository profesorRepository;
 
     public List<Materia> obtenerTodos() {
         return materiaRepository.findAll();
@@ -35,5 +40,22 @@ public class MateriaService {
     }
     public void eliminar(Long id) {
         materiaRepository.deleteById(id);
+    }
+    public void asignarProfesor(Long idMateria, Long idProfesor) {
+        Optional<Materia> materiaOptional = materiaRepository.findById(idMateria);
+        Optional<Profesor> profesorOptional = profesorRepository.findById(idProfesor);
+        if (materiaOptional.isPresent() && profesorOptional.isPresent()) {
+            Materia materia = materiaOptional.get();
+            Profesor profesor = profesorOptional.get();
+            List<Profesor> profesoresMateria = materia.getProfesores();
+            if (profesoresMateria == null) {
+                profesoresMateria = new ArrayList<>();
+            }
+            profesoresMateria.add(profesor);
+            materia.setProfesores(profesoresMateria);
+            materiaRepository.save(materia);
+        } else {
+            throw new IllegalArgumentException("No se encontró la materia o el profesor.");
+        }
     }
 }
