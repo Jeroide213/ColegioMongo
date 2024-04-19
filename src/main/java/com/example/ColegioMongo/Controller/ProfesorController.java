295d432e254543e3daa170aae371837e05600d21
Jeroide213 <153406693+Jeroide213@users.noreply.github.com>
2024-04-19 14:24:47 -0300

    First Commit

--- src/main/java/com/example/ColegioMongo/Controller/ProfesorController.java
+++ src/main/java/com/example/ColegioMongo/Controller/ProfesorController.java
@@ -2,7 +2,6 @@ package com.example.ColegioMongo.Controller;
 
 import com.example.ColegioMongo.Models.Profesor;
 import com.example.ColegioMongo.Service.ProfesorService;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
